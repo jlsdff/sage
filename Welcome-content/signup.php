258b4cf998922a5b380fad8f640702c802dd5648
julius <8215455@ntc.edu.ph>
2024-06-20 01:10:16 +0800

    changes

--- Welcome-content/signup.php
+++ Welcome-content/signup.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sign up</title>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
   <link rel="stylesheet" href="../css/output.css">
 </head>
 
@@ -13,7 +14,7 @@
   <main class="flex items-center justify-center w-screen min-h-screen px-8 sm:px-32">
     <section class="w-full min-h-[500px] flex flex-col sm:flex-row ">
       <!-- Form -->
-      <div class="w-1/2">
+      <div class="flex flex-col items-start justify-center w-full sm:w-1/2">
         <div class="w-[200px]">
           <img src="../pic/sagelogo.png" alt="Logo" class="w-full">
         </div>
@@ -24,7 +25,7 @@
             <br> choices that benefit both you and the planet.
           </p>
         </div>
-        <form class="mt-4" action="" method="post">
+        <form class="w-full mt-4" action="" method="post">
 
           <div>
             <?php
@@ -42,31 +43,32 @@
           </div>
 
           <div class="flex flex-col w-full gap-2">
-            <div class="max-w-lg">
+            <div class="w-full sm:max-w-lg">
               <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">Your name</label>
               <input type="name" id="name" placeholder="Enter your name"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                 required />
             </div>
-            <div class="max-w-lg">
+            <div class="w-full sm:max-w-lg">
               <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Email</label>
               <input type="email" id="email" placeholder="Email"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                 required />
             </div>
-            <div class="max-w-lg">
+            <div class="w-full sm:max-w-lg">
+              <!-- TODO: Make password verification -->
               <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
               <input type="password" id="password" placeholder="password"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                 required />
             </div>
-            <div class="max-w-lg">
+            <div class="w-full sm:max-w-lg">
               <label for="cpassword" class="block mb-2 text-sm font-medium text-gray-900 ">Confirm Password</label>
               <input type="password" id="cpassword" placeholder="Confirm password"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                 required />
             </div>
-            <div class="max-w-lg mt-4">
+            <div class="w-full mt-4 sm:max-w-lg">
               <button type="submit"
                 class="w-full text-center py-2.5 bg-primary-100 text-white font-bold hover:bg-primary-200 rounded-full">
                 Sign Up
@@ -74,24 +76,43 @@
             </div>
           </div>
         </form>
-        <div class="max-w-lg mt-4 text-center">
+        <div class="sm:w-[512px] mt-4 text-center">
           <p class="text-base text-neutral-400">Already have an account? <span
-              class="text-base font-bold text-primary-100 hover:underline "> <a href="">Login</a> </span> </p>
+              class="text-base font-bold text-primary-100 hover:underline ">
+              <!-- TODO: ROUTE -->
+              <a href="">Login</a> </span> </p>
           <br>
           <p class="text-base text-neutral-400">By continuing, you agree to our
             <br>
+            <!-- TODO: ROUTE -->
             <span class="text-base font-bold text-primary-100 hover:underline "><a href="">Terms of Service</a></span>
             and <span class="text-base font-bold text-primary-100 hover:underline "><a href="">Privacy Policy</a></span>
           </p>
         </div>
-      </div>
+      </div> 
       <!-- Carousel -->
-      <div class="w-1/2">
-
+      <div class="hidden w-1/2 sm:block">
+        <!-- Carousel Container-->
+        <div class="w-full p-8" id="default-carousel" class="relative w-full" data-carousel="slide">
+          <div class="relative w-full overflow-hidden rounded-lg h-[700px]">
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+              <img src="../pic/p1.png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt="...">
+            </div>
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+              <img src="../pic/p2.png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt="...">
+            </div>
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+              <img src="../pic/p3.png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt="...">
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </main>
-
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
 </body>
 
 </html>
